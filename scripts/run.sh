dff1bed3eb71bd54295a7cdd42169953d92f02b5
pedrofnseca <pedropereira.pp264@gmail.com>
2024-09-05 02:45:31 -0300

    chore: Update run.sh to compile all source files

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-mkdir -p bin
+# Diretórios
+SRC_DIR="src"
+BIN_DIR="bin"
 
-gcc src/main.c -o bin/out $(pkg-config --libs --cflags allegro-5 allegro_font-5 allegro_image-5 allegro_primitives-5 allegro_ttf-5 allegro_audio-5 allegro_acodec-5)
+# Arquivos fonte
+SRCS="$SRC_DIR/main.c $SRC_DIR/helper.c $SRC_DIR/draw.c $SRC_DIR/handle.c"
 
-./bin/out
+# Nome do executável
+TARGET="$BIN_DIR/out"
+
+# Flags do compilador
+CFLAGS=$(pkg-config --cflags allegro-5 allegro_font-5 allegro_image-5 allegro_primitives-5 allegro_ttf-5 allegro_audio-5 allegro_acodec-5)
+LDFLAGS=$(pkg-config --libs allegro-5 allegro_font-5 allegro_image-5 allegro_primitives-5 allegro_ttf-5 allegro_audio-5 allegro_acodec-5)
+
+# Criar diretório bin se não existir
+if [ ! -d "$BIN_DIR" ]; then
+  mkdir -p "$BIN_DIR"
+fi
+
+# Compilar os arquivos fonte
+gcc $SRCS -o $TARGET $CFLAGS $LDFLAGS
+
+# Verificar se a compilação foi bem-sucedida
+if [ $? -eq 0 ]; then
+  echo "Compilation succeeded!"
+  # Executar o programa
+  ./$TARGET
+else
+  echo "Compilation failed!"
+  exit 1
+fi
