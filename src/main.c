1ea3cc20c8725a44e8ce5729fa6a2a2bd446b622
pedrofnseca <pedropereira.pp264@gmail.com>
2024-09-15 02:22:44 -0300

    fix: remove unnecessary Allegro library includes

--- src/main.c
+++ src/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <allegro5/allegro5.h>
-#include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
 
 #include "headers/helper.h"
